cf4cafc7c30c1b289f22d5e939e9cba1fc242cca
izuum <dstep.ds@gmail.com>
2022-01-14 16:26:32 +0300

    add checking condition of ship(wrecked or sunken) when fired

--- src/main/java/GameProcess.java
+++ src/main/java/GameProcess.java
@@ -8,20 +8,20 @@ public class GameProcess {
     private int player2ShipCount;
 
     public GameProcess() {
-        this.player1ShipCount = 2;
-        this.player2ShipCount = 2;
+        this.player1ShipCount = 4;
+        this.player2ShipCount = 4;
 
         gameIsOn = true;
         isPlayer = Math.random() >= 0.5;
 
     }
 
-    public void playerTurn(GameField player1, GameField player2){ //
-            while (checkCountShips(player1ShipCount, player1)) {
+    public void playerTurn(GameField player1, GameField player2){
+            while (!checkCountShips(player1ShipCount, player1)) {
                 System.out.printf("%s Выставь корабли заново!", player1.getName());
                 player1.insertShips();
             }
-            while (checkCountShips(player2ShipCount, player2)) {
+            while (!checkCountShips(player2ShipCount, player2)) {
                 System.out.printf("%s Выставь корабли заново!", player2.getName());
                 player2.insertShips();
             }
@@ -66,31 +66,73 @@ public class GameProcess {
             }
         }
         if (countShips == playerShipCount){
-            return false;
+            return true;
         }else{
             System.out.printf("На поле игрока %s установлены не все корабли!", player.getName());
-            return true;
+            return false;
         }
     }
 
     private boolean hitShip(boolean isPlayer, String userInput, GameField player){//удар по кораблю
         String[] xy = userInput.split(",");
-        int x = Integer.parseInt(xy[0]);
-        int y = Integer.parseInt(xy[1]);
-        if(player.getPlayerField()[x][y]==1) {
-            System.out.println("Ранил!");
-            player.getPlayerField()[x][y] = -2;
-            if (isPlayer) {
-                player2ShipCount--;
-            } else {
-                player1ShipCount--;
+        if(player.getPlayerField()[Integer.parseInt(xy[0])][Integer.parseInt(xy[1])]==1) {
+            player.getPlayerField()[Integer.parseInt(xy[0])][Integer.parseInt(xy[1])] = -2;
+            if(shipSank(player.getPlayerField(), xy)) {
+                System.out.println("Утопил!");
+                if (isPlayer) {
+                    player2ShipCount--;
+                } else {
+                    player1ShipCount--;
+                }
+            }else{
+                System.out.println("Ранил");
             }
-        }else if(player.getPlayerField()[x][y]==-2) {
-            System.out.println("Ты уже попал сюда!");
+        }else if(player.getPlayerField()[Integer.parseInt(xy[0])][Integer.parseInt(xy[1])]==-2) {
+            System.out.println("Ты уже попал сюда! Ходи еще раз!");
         }else{
             System.out.println("Мимо!");
             isPlayer = !isPlayer;
         }
         return isPlayer;
+    }
+    private boolean shipSank(int[][] playerField, String[] hitCoordinates){
+        int x = Integer.parseInt(hitCoordinates[0]);
+        int y = Integer.parseInt(hitCoordinates[1]);
+        while (playerField[x][y] != 0) {
+            if(x > 0) {
+                x -= 1;
+                if (playerField[x][y] == 1) {
+                    return false;
+                }
+            }
+        }
+        x = Integer.parseInt(hitCoordinates[0]);
+        while (playerField[x][y] != 0) {
+            if(x < 9) {
+                x += 1;
+                if (playerField[x][y] == 1) {
+                    return false;
+                }
+            }
+        }
+        x = Integer.parseInt(hitCoordinates[0]);
+        while (playerField[x][y] != 0) {
+            if(y > 0){
+                y -= 1;
+                if (playerField[x][y] == 1) {
+                    return false;
+                }
+            }
+        }
+        y = Integer.parseInt(hitCoordinates[0]);
+        while (playerField[x][y] != 0) {
+            if(y < 9) {
+                y += 1;
+                if (playerField[x][y] == 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
